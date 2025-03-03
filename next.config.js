3a05e50e5cba8a167ea67b8466e875475801984b
tmvls123 <tmvls123@gmail.com>
2025-03-03 17:50:26 +0900

    fix: Vercel 배포를 위한 next.config.js 설정 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/holdom',
+  assetPrefix: '/holdom/',
 }
 
 module.exports = nextConfig 
