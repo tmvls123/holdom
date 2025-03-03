d31fd0c67c5000eaa6095b478ebc0d7ec710e659
tmvls123 <tmvls123@gmail.com>
2025-03-03 17:48:00 +0900

    fix: next.config.js 설정 수정 및 개발 환경 설정

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/holdom',
-  assetPrefix: '/holdom/',
-  trailingSlash: true,
-  distDir: 'out',
 }
 
 module.exports = nextConfig 
